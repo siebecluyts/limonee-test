c10f2694eb4d168640923b077b912b689c0e4486
Glitch (limonee) <none>
2025-04-08 11:06:54 +0000

    🎒👕 Checkpoint ./server.js:69254168/630 ./public/game/index.html:69254168/10785 ./designs.json:69254168/3 ./public/styles.css:69254168/2287 ./public/view.html:69254168/1747 ./package.json:69254168/1904

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const dataFile = './designs.json';
 
 // Middleware
 app.use(express.json());
@@ -13,6 +15,26 @@ app.use(express.static('public'));
 // 404 handler voor onbekende routes
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, '/404.html'));
+});
+if (!fs.existsSync(dataFile)) fs.writeFileSync(dataFile, '{}');
+
+app.post('/save', (req, res) => {
+  const design = req.body;
+  const id = uuidv4();
+  const data = JSON.parse(fs.readFileSync(dataFile));
+  data[id] = design;
+  fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
+  res.json({ id });
+});
+
+app.get('/design/:id', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataFile));
+  const design = data[req.params.id];
+  if (design) {
+    res.json(design);
+  } else {
+    res.status(404).json({ error: 'Design not found' });
+  }
 });
 
 // Start the server
