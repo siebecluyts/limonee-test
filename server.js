7ca1a0decbc2ee00991583ccbf3efc5a3fcfca28
Glitch (limonee) <none>
2025-04-28 18:34:04 +0000

    🗾🐛 Checkpoint ./server.js:69254168/2671

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-// EJS als view engine
+// EJS view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Public folder voor styles, scripts, images, etc
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Homepagina
@@ -15,25 +15,35 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Dynamische route handler
-app.get('/*', (req, res, next) => {
-  const urlPath = req.path; // Bijvoorbeeld: /about/personen/ReindertJanssens
-  const parts = urlPath.split('/').filter(Boolean); // ['about', 'personen', 'ReindertJanssens']
+// Dynamische route
+app.get('/*', (req, res) => {
+  const urlPath = req.path; // bijvoorbeeld: /about/personen
+  const parts = urlPath.split('/').filter(Boolean); // ['about', 'personen']
 
+  // Eerste: check of er een .ejs file direct bestaat
   let filePath = path.join(__dirname, 'views', ...parts) + '.ejs';
 
   fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      // Bestand bestaat niet -> 404
-      res.status(404).render('404');
-    } else {
-      // Bestand bestaat -> renderen
+    if (!err) {
+      // Bestaat direct als bestand
       res.render(parts.join('/'));
+    } else {
+      // Tweede: check of het een map is met index.ejs erin
+      let folderPath = path.join(__dirname, 'views', ...parts, 'index.ejs');
+      fs.access(folderPath, fs.constants.F_OK, (folderErr) => {
+        if (!folderErr) {
+          // Bestaat als map/index.ejs
+          res.render(path.join(parts.join('/'), 'index'));
+        } else {
+          // Bestaat niet -> 404
+          res.status(404).render('404');
+        }
+      });
     }
   });
 });
 
-// 404 fallback als niets matcht
+// 404 fallback
 app.use((req, res) => {
   res.status(404).render('404');
 });
