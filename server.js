7e40c3d39c5910c3049d2c1c125fc49b4136afb2
Glitch (limonee) <none>
2025-04-28 16:39:10 +0000

    🎪⛷ Checkpoint ./server.js:69254168/974 ./vieuws/game/index.ejs:69254168/7

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+// Zeg tegen Express dat je EJS gebruikt
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(express.json());
 app.use(express.static('public'));
-// 404 handler voor onbekende routes
+
+// 404 handler
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '/404.ejs'));
+    res.status(404).render('404');
 });
 
-// Start the server
+// Start server
 app.listen(PORT, () => {
-    console.log("Server running on port ${PORT}");
+    console.log(`Server running on port ${PORT}`);
 });
