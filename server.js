b15cdf1e72278e125e9984eef76e8be7b3a6c397
Glitch (limonee) <none>
2025-04-28 17:02:40 +0000

    ❄️👑 Checkpoint ./server.js:69254168/3552 ./views/about/personen/ArntJanssens/index.ejs:69254168/2253 ./views/about/personen/LodewijkVandueren/index.ejs:69254168/2245 ./views/about/personen/ReindertJanssens/index.ejs:69254168/2258 ./views/about/personen/SiebeCluyts/index.ejs:69254168/2229 ./views/partials/header.ejs:69254168/188 ./views/about/personen/TijlCluyts/index.ejs:69254168/2254 ./views/about/personen/index.ejs:69254168/2108 ./views/about/index.ejs:69254168/2244 ./views/badges/index.ejs:69254168/2058 ./views/contact/bestelling/index.ejs:69254168/1646 ./views/contact/index.ejs:69254168/2141 ./views/easter-egg/index.ejs:69254168/1865 ./views/game/index.ejs:69254168/2074 ./views/limonade/index.ejs:69254168/2244 ./views/photos/index.ejs:69254168/2074

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Zeg tegen Express dat je EJS gebruikt
+// Instellingen voor EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -11,12 +13,33 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Automatische route-handler
+app.get('/:page?', (req, res, next) => {
+    const page = req.params.page;
+
+    if (!page) {
+        // Als geen pagina opgegeven ➔ home pagina
+        return res.render('index');
+    }
+
+    const filePath = path.join(__dirname, 'views', page, 'index.ejs');
+
+    // Check of de folder + index.ejs bestaat
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            next(); // niet gevonden ➔ naar 404
+        } else {
+            res.render(`${page}/index`);
+        }
+    });
+});
+
 // 404 handler
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.status(404).render('404');
 });
 
-// Start server
+// Server starten
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
