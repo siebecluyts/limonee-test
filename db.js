99f77a93e00addb5309376afe45da324ceca8ea2
SiebeCluyts <debendevanzelem1@gmail.com>
2025-06-03 20:27:11 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
   ssl: {
     rejectUnauthorized: false,
   },
